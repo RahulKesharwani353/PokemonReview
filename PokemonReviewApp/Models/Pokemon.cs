3b89940512ee64f774791d4b174f6e2ea7f536fc
RahulKesharwani353 <rahulkesharwani353@gmail.com>
2023-02-03 15:44:35 +0530

    initial commit

---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PokemonReviewApp.Models
+{
+	public class Pokemon
+	{
+		public int poke_id { get; set; }
+		public string? name { get; set; }
+		public string? poke_type { get; set; }
+
+	}
+}
+

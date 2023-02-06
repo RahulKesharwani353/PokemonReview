ad3b13422993c7d16ec356120808b85ae949bbba
RahulKesharwani353 <rahulkesharwani353@gmail.com>
2023-02-06 10:23:05 +0530

    model class created

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -4,7 +4,7 @@ namespace PokemonReviewApp.Models
 	public class Pokemon
 	{
 		public int poke_id { get; set; }
-		public string? name { get; set; }
+		public string? name { get; set; }	
 		public string? poke_type { get; set; }
 
 	}

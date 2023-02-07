f725fb982379dbd9bd7954e33029ce539ac41960
RahulKesharwani353 <rahulkesharwani353@gmail.com>
2023-02-07 22:15:47 +0530

    DbContext and DBConnection added

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -3,10 +3,10 @@ namespace PokemonReviewApp.Models
 {
 	public class Pokemon
 	{
-		public int poke_id { get; set; }
-		public string? name { get; set; }	
-		public string? poke_type { get; set; }
-		public ICollection<Review>? reviews { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
 
--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,13 +1,39 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<Seed>();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(opt =>
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddDbContext<DataContext>(
+//        opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+//    );
 
 var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
